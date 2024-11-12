719566887b853b233297abdfeab61ddc1c3639b1
nagarjun526 <nagarjun3147@gmail.com>
2024-11-12 10:32:12 +0530

    Create Main.java

---
+++ EB_DSA/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+class Sum{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c;
+    c=a+b;
+    System.out.println(c);
+    c=a-b;
+    System.out.println(c);
+    c=a*b;
+    System.out.println(c);
+    c=a/b;
+    System.out.println(c);
+    c=a%b;
+    System.out.println(c);
+  }
+}
